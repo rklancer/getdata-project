26f1595c772a9a14e0daf9bf17e96a3577d14c71
Richard Klancer <rpk@pobox.com>
2014-04-26 10:58:58 -0400

    Working merged dataset, but not using dplyr

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 # Uses the consistent naming pattern found in the dataset to find features of a particular type,
 # such as "mean" or "std". The trick is that features representing a mean are named like "*-mean()*"
 # so we grep for features having that pattern:
@@ -18,20 +20,28 @@ extractAndNameFeatures <- function(data, desiredFeatures) {
     ret
 }
 
-read <- function(type) {
-    ret <- extractAndNameFeatures(readFeatureMatrix(file.path(type, paste('X_', type, '_head.txt', sep=''))), desiredFeatures)
-    ret$type = as.factor(type)
-    ret
+pathFor <- function(prefix, class) {
+    file.path(class, paste(prefix, '_', class, '_head', '.txt', sep=''))
+}
+
+read <- function(class) {
+    cbind(
+        subject      = scan(pathFor('subject', class)),
+        activityCode = scan(pathFor('y', class)),
+        class        = as.factor(class),
+        extractAndNameFeatures(readFeatureMatrix(pathFor('X', class)), desiredFeatures)
+    )
 }
 
 # Read in feature names. The data in features.txt amount to the nonexistent column headers for the
 # files test/X_test.txt and train/X_train.txt
 features <- read.delim('features.txt', sep=' ', header=FALSE,
                 col.names=c('index', 'name'),
-                colClasses=c("integer", "character"))
+                colClasses=c('integer', 'character'))
 
 meanFeatures <- features[ findIndicesOfFeatureType('mean', features$name), ]
 stdFeatures  <- features[ findIndicesOfFeatureType('std', features$name), ]
 desiredFeatures <- rbind(meanFeatures, stdFeatures)
 
 X_head <- rbind(read('test'), read('train'))
+
